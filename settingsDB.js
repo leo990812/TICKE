13097f8f791e4c870e41c54cc8087d7cee0bf34b
leo990812 <106399653+leo990812@users.noreply.github.com>
2025-10-11 10:07:52 +0800

    Update settingsDB.js

--- settingsDB.js
+++ settingsDB.js
@@ -1,10 +1,11 @@
 // settingsDB.js
-// 用來存每個伺服器的設定
 module.exports = {
-  // 範例：伺服器ID: {notifyRole, supportRole, ticketChannel}
+  // 範例：伺服器ID
   "伺服器ID1": {
-    notifyRole: "普通成員角色ID",
-    supportRole: "工單控制權限角色ID",
-    ticketChannel: "按鈕發送頻道ID"
+    notifyRole: "普通成員角色ID",         // @提醒角色
+    supportRole: "工單控制權限角色ID",     // 支援角色
+    ticketChannel: "按鈕發送頻道ID",       // 按鈕頻道
+    logChannel: "紀錄上傳頻道ID",         // 保存紀錄頻道
+    welcomeMessage: "📩 歡迎！請描述你的問題", // 開啟工單時的訊息內容
   }
 };
