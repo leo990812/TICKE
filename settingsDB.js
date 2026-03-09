744b3505ffe8bf196021d510a1f9348b0f176bef
leo990812 <106399653+leo990812@users.noreply.github.com>
2026-03-09 22:13:54 +0800

    Update settingsDB.js

--- settingsDB.js
+++ settingsDB.js
@@ -1,11 +1,9 @@
-// settingsDB.js
 module.exports = {
-  // 範例：伺服器ID
-  "伺服器ID1": {
-    notifyRole: "普通成員角色ID",         // @提醒角色
-    supportRole: "工單控制權限角色ID",     // 支援角色
-    ticketChannel: "按鈕發送頻道ID",       // 按鈕頻道
-    logChannel: "紀錄上傳頻道ID",         // 保存紀錄頻道
-    welcomeMessage: "📩 歡迎！請描述你的問題", // 開啟工單時的訊息內容
-  }
+    "你的伺服器ID": {
+        "notifyRole": "123456789012345678",
+        "supportRole": "123456789012345678",
+        "ticketChannel": "123456789012345678",
+        "logChannel": "123456789012345678",
+        "welcomeMessage": "📩 歡迎！請描述你的問題，支援小組將盡快為您服務。"
+    }
 };
